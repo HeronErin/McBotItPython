addca900160016d0a7e834aec1763be500e4a110
pickle <you@example.com>
2023-05-18 08:40:12 -0400

    fixed __init__.py

--- mcbotit/__init__.py
+++ mcbotit/__init__.py
@@ -1,1 +1,4 @@
 VERSION = "1.0"
+from .client import *
+from .player import *
+from .inventoryHelper import *
--- mcbotit/player.py
+++ mcbotit/player.py
@@ -1,5 +1,5 @@
-from client import Client
-from inventoryHelper import *
+from .client import Client
+from .inventoryHelper import *
 class Player:
 	def __init__(self, port, keepAliveTime=0.1):
 		self.clientConnection = Client(keepAliveTime, port)
